8e2ab4ebd4865dd0fc3489a3052f3eefc48a7aef
clouddevnet <126808165+clouddevnet@users.noreply.github.com>
2024-03-30 19:14:32 -0400

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 st.title("Stock Tracking Dashboard")
 
-option = st.sidebar.selectbox("Watchlist?", ('Daily_review', 'CURRENT', 'ETF', 'DWM'))
+option = st.sidebar.selectbox("Watchlist?", ('Daily_review', 'CURRENT', 'ETF', 'MASTER', 'DWM'))
 
 timing = st.sidebar.selectbox("What Timeframe?", ('Daily', 'Weekly', 'Multi'))
 
@@ -55,6 +55,26 @@ if option == "CURRENT":
 
 if option == "ETF":
     with open('etf') as f:
+        symbols=[i.strip() for i in f.readlines()]
+        symbols = list(dict.fromkeys(symbols))
+
+    if timing == "Daily":
+        for stock in symbols:
+            st.image(f"https://charts2-node.finviz.com/chart.ashx?cs=l&t={stock}&tf=d&s=linear&ct=candle_stick&o[0][ot]=sma&o[0][op]=50&o[0][oc]=FF8F33C6&o[1][ot]=ema&o[1][op]=10&o[1][oc]=DCB3326D&o[2][ot]=ema&o[2][op]=21&o[2][oc]=DC32B363&o[3][ot]=patterns&o[3][op]=&o[3][oc]=000")
+
+
+    if timing == "Weekly":
+        for stock in symbols:
+            st.image(f"https://charts2-node.finviz.com/chart.ashx?cs=l&t={stock}&tf=w&s=linear&ct=candle_stick&o[0][ot]=sma&o[0][op]=40&o[0][oc]=FF8F33C6&o[1][ot]=sma&o[1][op]=30&o[1][oc]=DCB3326D&o[2][ot]=sma&o[2][op]=10&o[2][oc]=DC32B363&o[3][ot]=patterns&o[3][op]=&o[3][oc]=000")
+
+    if timing == "Multi":
+        for stock in symbols:
+            st.image(f"https://charts2-node.finviz.com/chart.ashx?cs=l&t={stock}&tf=m&s=linear&ct=candle_stick&o[0][ot]=sma&o[0][op]=5&o[0][oc]=FF8F33C6&o[1][ot]=sma&o[1][op]=10&o[1][oc]=DCB3326D&o[2][ot]=sma&o[2][op]=20&o[2][oc]=DC32B363&o[3][ot]=patterns&o[3][op]=&o[3][oc]=000")
+            st.image(f"https://charts2-node.finviz.com/chart.ashx?cs=l&t={stock}&tf=w&s=linear&ct=candle_stick&o[0][ot]=sma&o[0][op]=40&o[0][oc]=FF8F33C6&o[1][ot]=sma&o[1][op]=30&o[1][oc]=DCB3326D&o[2][ot]=sma&o[2][op]=10&o[2][oc]=DC32B363&o[3][ot]=patterns&o[3][op]=&o[3][oc]=000")
+            st.image(f"https://charts2-node.finviz.com/chart.ashx?cs=l&t={stock}&tf=d&s=linear&ct=candle_stick&o[0][ot]=sma&o[0][op]=50&o[0][oc]=FF8F33C6&o[1][ot]=ema&o[1][op]=10&o[1][oc]=DCB3326D&o[2][ot]=ema&o[2][op]=21&o[2][oc]=DC32B363&o[3][ot]=patterns&o[3][op]=&o[3][oc]=000")
+
+if option == "MASTER":
+    with open('master') as f:
         symbols=[i.strip() for i in f.readlines()]
         symbols = list(dict.fromkeys(symbols))
 
