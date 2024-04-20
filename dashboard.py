13f09af3e00c32160b11c9babf4b4444fba9bb3d
clouddevnet <126808165+clouddevnet@users.noreply.github.com>
2024-04-20 15:20:33 -0400

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -11,14 +11,14 @@ st.set_page_config(
 
 #st.title("Stock Tracking Dashboard")
 
-option = st.sidebar.selectbox("Watchlist?", ('Daily_review', 'WEEKLY_WL', 'ETF', 'SPQQ100', 'NEW_Daily_review'))
+option = st.sidebar.selectbox("Watchlist?", ('Daily_review', 'Current_watchlist', 'ETF', 'SPQQ100', 'Old_Daily_review'))
 
 timing = st.sidebar.selectbox("What Timeframe?", ('Daily', 'Weekly', 'Multi'))
 
 st.header(option)
 
 
-if option == "Daily_review":
+if option == "Old_Daily_review":
     symbols=['SPY', 'QQQ', 'DIA',  'IWO', 'VTI', 'RSP', 'QQQE', 'SDS', 'XLK', 'SMH', 'IGV', 'CIBR', 'VGT', 'TAN', 'FFTY', 'IBIT', 
              'XLF', 'XLE', 'XLP', 'XLV', 'XLI', 'XLK', 'XLC', 'XLU', 'XLY', 'XLB', 'XOP', 'XHB', 'XME', 'OIH', 'ITB', 'PAVE', 'JETS', 'XBI', 'GDX', 'GDXJ',
              'ARKK', 'BLOK', 'AIQ', 'WCLD', 'BOTZ', 'IPAY',  'SOXL', 'SSO', 'QLD', 'TQQQ', 'SQQQ', 'FNGU', 'XLG', 'IVV', 'IJH', 'IJR', 'IDEV', 'IEMG', 'AGG', 'TLT', 'FTEC', 'FDN',
@@ -42,7 +42,7 @@ if option == "Daily_review":
             st.image(f"https://charts2-node.finviz.com/chart.ashx?cs=l&t={stock}&tf=d&s=linear&ct=candle_stick&o[0][ot]=sma&o[0][op]=50&o[0][oc]=FF8F33C6&o[1][ot]=ema&o[1][op]=10&o[1][oc]=DCB3326D&o[2][ot]=ema&o[2][op]=21&o[2][oc]=DC32B363&o[3][ot]=patterns&o[3][op]=&o[3][oc]=000")
 
 
-if option == "WEEKLY_WL":
+if option == "Current_watchlist":
     with open('daily') as f:
         symbols=[i.strip() for i in f.readlines()]
 
@@ -106,7 +106,7 @@ if option == "SPQQ100":
             st.image(f"https://charts2-node.finviz.com/chart.ashx?cs=l&t={stock}&tf=w&s=linear&ct=candle_stick&o[0][ot]=sma&o[0][op]=40&o[0][oc]=FF8F33C6&o[1][ot]=sma&o[1][op]=30&o[1][oc]=DCB3326D&o[2][ot]=sma&o[2][op]=10&o[2][oc]=DC32B363&o[3][ot]=patterns&o[3][op]=&o[3][oc]=000")
             st.image(f"https://charts2-node.finviz.com/chart.ashx?cs=l&t={stock}&tf=d&s=linear&ct=candle_stick&o[0][ot]=sma&o[0][op]=50&o[0][oc]=FF8F33C6&o[1][ot]=ema&o[1][op]=10&o[1][oc]=DCB3326D&o[2][ot]=ema&o[2][op]=21&o[2][oc]=DC32B363&o[3][ot]=patterns&o[3][op]=&o[3][oc]=000")
 
-if option == "NEW_Daily_review":
+if option == "Daily_review":
     st.write("MARKET RESEARCH")
 
     url = "https://finviz.com/groups.ashx?g=sector&v=210&o=name"
